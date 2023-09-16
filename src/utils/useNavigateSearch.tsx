13e3f4fc7af85d3402deb4444fe0509e6b01850d
Alizeh Khan <alizehedinburgh@gmail.com>
2023-09-16 10:55:34 +0200

    fix: params type

--- src/utils/useNavigateSearch.tsx
+++ src/utils/useNavigateSearch.tsx
@@ -1,8 +1,9 @@
 import { createSearchParams, useNavigate } from 'react-router-dom'
+import type { URLSearchParamsInit } from 'react-router-dom'
 
 const useNavigateSearch = () => {
   const navigate = useNavigate()
-  return (pathname: string, params: object) =>
+  return (pathname: string, params: URLSearchParamsInit | undefined) =>
     navigate({ pathname, search: `?${createSearchParams(params)}` })
 }
 
