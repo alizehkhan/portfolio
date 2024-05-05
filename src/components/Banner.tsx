2b921e921a8813976558520173e33d253e16ce06
Alizeh Khan <alizehedinburgh@gmail.com>
2024-05-05 09:55:34 +0200

    fix: banner flex shrink

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,10 @@
-import { IconInfoCircle } from '@tabler/icons-react'
+import { IconBulb } from '@tabler/icons-react'
 import { ReactNode } from 'react'
 
 export const Banner = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className="flex gap-4 items-center text-base bg-green-300 rounded-lg py-2 px-4 shadow-sm my-4">
-			<IconInfoCircle color="#2e6b34" size={40} />
+		<div className="flex gap-3 text-base items-center bg-green-300 rounded-lg py-2 px-4 shadow-sm my-4">
+			<IconBulb color="#2e6b34" size={20} className="shrink-0" />
 			<p className="text-green-700">{children}</p>
 		</div>
 	)
