80d6fa1a77bd1e4c3111e30940565986f04beb47
Alizeh Khan <alizehedinburgh@gmail.com>
2024-07-04 15:20:37 +0200

    feat: add link to app cards

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,8 +1,21 @@
-export const AppCard = ({ image, name }: { image: string; name: string }) => {
+import { Link } from 'react-router-dom'
+
+export const AppCard = ({
+	image,
+	name,
+	link,
+}: {
+	image: string
+	name: string
+	link?: string
+}) => {
 	return (
-		<div className="-mb-2 mt-3 flex w-fit items-center gap-3 rounded-md bg-white p-2 shadow-sm">
+		<Link
+			to={link ?? '#'}
+			className="-mb-2 mt-3 flex w-fit items-center gap-3 rounded-md bg-white p-2 shadow-sm"
+		>
 			<img className="h-10 w-10 rounded-md object-cover" src={image} alt="" />
 			<p className="text-md font-serif">{name}</p>
-		</div>
+		</Link>
 	)
 }
